6fec682d0fe5f0197f9fea9b6d3479908acdca86
Kayke Alves <alveskayke033@gmail.com>
2023-01-31 15:35:52 -0300

    feat: Add core components

---
+++ README.md
@@ -0,0 +1,10 @@
+# Components
+
+- [ ] Text
+- [ ] Heading
+- [ ] Box
+- [ ] Button
+- [ ] TextInput
+- [ ] TextArea
+- [ ] Avatar
+- [ ] MultiStep
---
+++ packages/docs/src/stories/Box.stories.tsx
@@ -0,0 +1,23 @@
+import type { StoryObj, Meta } from '@storybook/react'
+import { Box, BoxProps } from '@ignite-ui/react'
+
+export default {
+  title: 'Surfaces/Box',
+  component: Box,
+  args: {
+    children: (
+      <>
+        <span>Testando...</span>
+      </>
+    ),
+  },
+  argTypes: {
+    children: {
+      control: {
+        type: null,
+      },
+    },
+  },
+} as Meta<BoxProps>
+
+export const Primary: StoryObj<BoxProps> = {}
